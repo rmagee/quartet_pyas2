49fb098ad3c1ed95499b28a43bbd8e4b06251130
Chuck <chuck@chucksailer.com>
2019-07-12 11:09:12 -0400

    Added Q4 Branding

--- quartet_pyas2/settings.py
+++ quartet_pyas2/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'quartet_pyas2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 PYAS = {
 
     'DATADIR' : PROJ_PATH,
